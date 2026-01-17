56db3094fe8087b7a03a31d577ca303f25d84b17
Joao Pedro <joaopedro7281818@gmail.com>
2026-01-17 01:13:15 -0300

    Primeiro código C# - Calculadora

---
+++ Program.cs
@@ -0,0 +1,63 @@
+using Calculadora;
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Escolha a operacao: Paridade, Soma, Subtracao, Multiplicacao, Divisao");
+            var operacao = Console.ReadLine();
+
+            if (operacao == "Paridade") {
+                var calc = new Operacoes();
+                Console.WriteLine("Digite um numero para ver se e par...");
+                int numero1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"O Resultado de {numero1} é : " + 
+                (calc.ParImpar(numero1) == 0 ? "Par" : "Impar"));
+            }
+            else if (operacao == "Soma")
+            {
+                var calc = new Soma();
+                Console.WriteLine("Digite o Primeiro número...");
+                int numero1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite o Segundo número...");
+                int numero2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"O Resultado de {numero1} + {numero2} é: " +calc.Somar(numero1, numero2));
+            }
+            else if (operacao == "Subtracao")
+            {
+                var calc = new Subtracao();
+                Console.WriteLine("Digite o Primeiro numero...");
+                int numero1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite o segundo numero...");
+                int numero2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"O resultado de {numero1} - {numero2} é: " + calc.Subtrair(numero1, numero2));
+            }
+            else if (operacao == "Multiplicacao") 
+            {
+                var calc = new Multiplicacao();
+                Console.WriteLine("Digite o primeiro numero...");
+                int numero1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite o segundo numero...");
+                int numero2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"O resultado de {numero1} * {numero2} é: " + calc.Multiplicar(numero1, numero2));
+            }
+            else if (operacao == "Divisao")
+            {
+                var calc = new Divisao();
+                Console.WriteLine("Digite o primeiro numero...");
+                int numero1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite o segundo numero...");
+                int numero2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"O resultado de {numero1} / {numero2} é: " + calc.Dividir(numero1, numero2));
+            }
+        }
+    }
+}
+
+/*Console.WriteLine("Digite seu nome:");
+            var Person = new User();
+            Person.Name = Console.ReadLine() ?? "";
+            Console.WriteLine("Digite sua idade:");
+            Person.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Bem-vindo, {Person.Name}, de {Person.Age}!");*/
---
+++ namespaces/calculator.cs
@@ -0,0 +1,42 @@
+namespace Calculadora
+{
+    public class Operacoes
+    {
+        public int ParImpar(int a)
+        {
+            return a % 2;
+        }
+    }
+
+    public class Soma
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class Subtracao
+    {
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    public class Multiplicacao
+    {
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    public class Divisao
+    {
+        public int Dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
